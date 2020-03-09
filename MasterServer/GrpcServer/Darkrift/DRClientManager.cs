666b917ea294f45f6e887875858aee0ecb039d12
Akhil Menon <menonakhilmenon@gmail.com>
2020-03-09 21:25:01 +0530

    Added Save Char data Message

--- MasterServer/GrpcServer/Darkrift/DRClientManager.cs
+++ MasterServer/GrpcServer/Darkrift/DRClientManager.cs
@@ -3,6 +3,8 @@ using MasterServer.Config;
 using Grpc.Net.Client;
 using MasterServer.DarkRift.Shared;
 using MasterServer.Darkrift.Shared.Models;
+using DarkRift.Client;
+using System;
 
 namespace MasterServer.DarkRift
 {
@@ -10,13 +12,42 @@ namespace MasterServer.DarkRift
     {
         private readonly ServerAddresses config;
         private readonly DRCommunicator communicator;
+        private readonly DRClientHelper helper;
 
-        public DRClientManager(ServerAddresses config, DRCommunicator communicator)
+        public DRClientManager(ServerAddresses config, DRCommunicator communicator, DRClientHelper helper)
         {
             this.config = config;
             this.communicator = communicator;
+            this.helper = helper;
+            Init();
         }
 
+        private void Init() 
+        {
+            helper.OnClientMessageReceived += HandleClientMessage;
+        }
+
+        private void HandleClientMessage(object sender, MessageReceivedEventArgs e)
+        {
+            switch ((MasterServerNoReplyTags)e.Tag)
+            {
+                case MasterServerNoReplyTags.SaveCharacterData: 
+                    {
+                        using (var message = e.GetMessage())
+                        {
+                            using(var reader = message.GetReader()) 
+                            {
+                                var mess = reader.ReadSerializable<ServerCharData>();
+                                var channel = GrpcChannel.ForAddress(config.UserServerAddress);
+                                var client = new UserService.Protos.UserManagement.UserManagementClient(channel);
+                                var charInfo = new UserService.Protos.CharacterInfo { CharID = mess.charID, CharData = mess.jsonData };
+                                client.SaveUserDataAsync(charInfo);
+                            }
+                        }
+                        break;
+                    }
+            }
+        }
 
         public async Task<ushort> UserLoggedIn(string userID)
         {
@@ -30,7 +61,9 @@ namespace MasterServer.DarkRift
                 return 0;
             }
 
-            var response = await communicator.SendMessageWithReply<ServerCharData,ServerAuthReply>(new ServerCharData {charID = res.CharID,jsonData = res.CharData }, (ushort)MasterServerReplyTags.CharacterLoggedIn);
+            var response = await communicator.SendMessageWithReply<ServerCharData,ServerAuthReply>(
+                new ServerCharData {charID = res.CharID,jsonData = res.CharData }, 
+                (ushort)MasterServerReplyTags.CharacterLoggedIn);
             return response.sessToken;
         }
     }
